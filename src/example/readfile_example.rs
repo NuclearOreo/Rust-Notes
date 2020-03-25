f0772829009f61b16986ac2a67dfd518f1eda250
Ussama Azam <nuclearoreo@gmail.com>
2020-03-25 17:44:08 -0400

    Adding reading file IO

--- src/example/readfile_example.rs
+++ src/example/readfile_example.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 pub fn run() {
-    println!("This is a test");
+    let mut file = File::open("./file.txt").expect("Can't find file");
+
+    let mut content = String::new();
+
+    file.read_to_string(&mut content).expect("Can't read file");
+
+    println!("{}", content);
 }
