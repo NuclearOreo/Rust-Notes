5c130ebfb436b97032d19a9313b656b365b617f3
Ussama Azam <nuclearoreo@gmail.com>
2020-03-25 17:07:43 -0400

    Getting rid of deleted fiels

--- src/tuples.rs
+++
@@ -1,9 +0,0 @@
-// Tuples group together values of different types
-// Max 12 elements
-
-pub fn run() {
-    let person: (&str, &str, i8) = ("Ussama", "Miami", 25);
-
-    println!("{:?}", person);
-    println!("{} from {}, age {}", person.0, person.1, person.2);
-}
